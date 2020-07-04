94ecdd5980d51e1c0e2b9f510ba764d5eba68824
Shuman Shen <s3735878@student.rmit.edu.au>
2020-07-04 21:35:20 +1000

    menu-item routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
-import HomePage from "./homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/shop/hats" component={HatsPage}></Route>
+        <Route path="/" component={HomePage}></Route>
+      </Switch>
     </div>
   );
 }
