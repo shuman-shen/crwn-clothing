21522835dd484145b13fee20ac3a38e18a0241cc
Shuman Shen <s3735878@student.rmit.edu.au>
2020-07-05 20:37:33 +1000

    shop with collection preview and items

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route path="/shop/hats" component={HatsPage}></Route>
-        <Route path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+        <Route exact path="/" component={HomePage}></Route>
       </Switch>
     </div>
   );
