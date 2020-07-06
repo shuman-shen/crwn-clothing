12d8416945e5fb2de21f43c96eb927ae43fe37e5
Shuman Shen <s3735878@student.rmit.edu.au>
2020-07-07 00:26:33 +1000

    sign in form with google signin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/shop" component={ShopPage}></Route>
-        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signin" component={SignInSignUp} />
       </Switch>
     </div>
   );
